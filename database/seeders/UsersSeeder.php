ae2bafb07d06cf551084ca116e0654bfeaa1f5ab
jumbleofcodes <45831879+jumbleofcodes@users.noreply.github.com>
2021-05-14 00:32:59 +0200

    feat: created UsersController.php, Installed Sanctum,created basic tables, created Role model

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'Admin';
+        $user->email = 'admin@admin.it';
+        $user->password = Hash::make('password');
+        $user->save();
+        // -> get() ritorna sempre una collection
+        // ->first() ritorna il primo oggetto corrispondente
+
+        $user->roles()->attach(Role::where('key',Role::ROLE_ADMIN)->first());
+
+        User::factory()->count(50)->create();
+    }
+}
